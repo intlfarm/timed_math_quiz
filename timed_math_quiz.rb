84c7d595ff17c4f1c7d051d6dfc4a48569360003
Hill <intlfarm@INTERNAONALsAir.home>
2016-12-31 21:19:11 -0500

    second commit

--- timed_math_quiz.rb
+++ timed_math_quiz.rb
@@ -1,16 +1,32 @@
 def timed_math_quiz
 
-start_time = Time.new.round(2)
+  puts "You must answer 10 questions as fast as you can! Go!\n"
 
-puts "Give me something"
-user_input = gets.chomp
+  start_time = Time.new.round(2)
+  count = []
 
+  i = 0
 
+  while i < 10
+    digit_one = rand(10)
+    digit_two = rand(10)
+    digit_answer = digit_one + digit_two
 
-end_time = Time.new.round(2)
+    puts "What is " + digit_one.to_s + " + " + digit_two.to_s
+    user_input = gets.chomp
 
-final_time = end_time - start_time
+    count << user_input.include?(digit_answer.to_s)
 
-puts "Your final time is " + final_time.to_s + " seconds"
+  i += 1
+  end
+
+  end_time = Time.new.round(2)
+  final_time = end_time - start_time
+
+  puts "Your final time is " + final_time.to_s + " seconds"
+  puts "Each true counts as 10 points! Add them up for your final score!"
+  puts count
 
 end
+
+timed_math_quiz
